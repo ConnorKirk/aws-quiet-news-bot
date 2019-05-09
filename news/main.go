d83b754e20639b526e67b18399fcb66d70047845
Connor Kirkpatrick <connor.kirkpatrick@hotmail.com>
2019-05-09 11:09:18 +0100

    Remove unused errors

--- news/main.go
+++ news/main.go
@@ -25,12 +25,6 @@ const (
 var (
 	timeWindow = 24 * time.Hour
 
-	// ErrNoIP No IP found in response
-	ErrNoIP = errors.New("No IP in HTTP response")
-
-	// ErrNon200Response non 200 status code in response
-	ErrNon200Response = errors.New("Non 200 Response found")
-
 	// Destination Webhook
 	webhookURL string
 )
